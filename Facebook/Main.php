2651d8f1f3e4a9c829193b609e540341b968cff8
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-03 17:18:48 +0100

    Posse select button

--- Facebook/Main.php
+++ Facebook/Main.php
@@ -21,84 +21,91 @@
             function registerEventHooks() {
                 // Push "notes" to Facebook
                 \Idno\Core\site()->addEventHook('post/note',function(\Idno\Core\Event $event) {
-                    $object = $event->data()['object'];
-                    if ($this->hasFacebook()) {
-                        if ($facebookAPI = $this->connect()) {
-                            $facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
-                            $message = strip_tags($object->getDescription());
-                            //$message .= "\n\n" . $object->getURL();
-                            if (!empty($message) && substr($message,0,1) != '@') {
-                                $params = array(
-                                    'message' => $message
-                                );
-                                if (preg_match('/(?<!=)(?<!["\'])((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$message,$matches)) {
-                                    $params['link'] = $matches[0];  // Set the first discovered link as the match
-                                }
-                                try {
-                                    $result = $facebookAPI->api('/me/feed', 'POST', $params);
-                                    if (!empty($result['id'])) {
-										$object->setPosseLink('facebook','https://facebook.com/' . $result['id']);
-										$object->save();
+					
+					if (in_array('facebook', $_REQUEST['posseMethod'])) { // TODO: Use the correct input functions
+						$object = $event->data()['object'];
+						if ($this->hasFacebook()) {
+							if ($facebookAPI = $this->connect()) {
+								$facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
+								$message = strip_tags($object->getDescription());
+								//$message .= "\n\n" . $object->getURL();
+								if (!empty($message) && substr($message,0,1) != '@') {
+									$params = array(
+										'message' => $message
+									);
+									if (preg_match('/(?<!=)(?<!["\'])((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$message,$matches)) {
+										$params['link'] = $matches[0];  // Set the first discovered link as the match
 									}
-                                } catch (\Exception $e) {
-                                    \Idno\Core\site()->session()->addMessage('There was a problem posting to Facebook: ' . $e->getMessage());
-                                }
-                            }
-                        }
-                    }
+									try {
+										$result = $facebookAPI->api('/me/feed', 'POST', $params);
+										if (!empty($result['id'])) {
+											$object->setPosseLink('facebook','https://facebook.com/' . $result['id']);
+											$object->save();
+										}
+									} catch (\Exception $e) {
+										\Idno\Core\site()->session()->addMessage('There was a problem posting to Facebook: ' . $e->getMessage());
+									}
+								}
+							}
+						}
+					}
                 });
 
                 // Push "articles" to Facebook
                 \Idno\Core\site()->addEventHook('post/article',function(\Idno\Core\Event $event) {
-                    $object = $event->data()['object'];
-                    if ($this->hasFacebook()) {
-                        if ($facebookAPI = $this->connect()) {
-                            $facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
-                            $result = $facebookAPI->api('/me/feed', 'POST',
-                                array(
-                                    'link' => $object->getURL(),
-                                    'message' => $object->getTitle()
-                                ));
-                            if (!empty($result['id'])) {
-								$object->setPosseLink('facebook','https://facebook.com/' . $response['id']);
-								$object->save();
+					if (in_array('facebook', $_REQUEST['posseMethod'])) { // TODO: Use the correct input functions
+						$object = $event->data()['object'];
+						if ($this->hasFacebook()) {
+							if ($facebookAPI = $this->connect()) {
+								$facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
+								$result = $facebookAPI->api('/me/feed', 'POST',
+									array(
+										'link' => $object->getURL(),
+										'message' => $object->getTitle()
+									));
+								if (!empty($result['id'])) {
+									$object->setPosseLink('facebook','https://facebook.com/' . $response['id']);
+									$object->save();
+								}
 							}
-                        }
-                    }
+						}
+					}
                 });
 
                 // Push "images" to Facebook
                 \Idno\Core\site()->addEventHook('post/image',function(\Idno\Core\Event $event) {
-                    $object = $event->data()['object'];
-                    if ($attachments = $object->getAttachments()) {
-                        foreach($attachments as $attachment) {
-                            if ($this->hasFacebook()) {
-                                if ($facebookAPI = $this->connect()) {
-                                    $facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
-                                    $message = strip_tags($object->getDescription());
-									//$message .= "\n\n" . $object->getURL();
-                                    try {
-                                        $facebookAPI->setFileUploadSupport(true);
-                                        $response = $facebookAPI->api(
-                                            '/me/photos/',
-                                            'post',
-                                            array(
-                                                'message' => $message,
-                                                'url' => $attachment['url']
-                                            )
-                                        );
-                                        if (!empty($response['id'])) {
-                                        	$object->setPosseLink('facebook','https://facebook.com/' . $response['id']);
-                                        	$object->save();
-                                        }
-                                    }
-                                    catch (\FacebookApiException $e) {
-                                        error_log('Could not post image to Facebook: ' . $e->getMessage());
-                                    }
-                                }
-                            }
-                        }
-                    }
+					if (in_array('facebook', $_REQUEST['posseMethod'])) { // TODO: Use the correct input functions
+						$object = $event->data()['object'];
+						if ($attachments = $object->getAttachments()) {
+							foreach($attachments as $attachment) {
+								if ($this->hasFacebook()) {
+									if ($facebookAPI = $this->connect()) {
+										$facebookAPI->setAccessToken(\Idno\Core\site()->session()->currentUser()->facebook['access_token']);
+										$message = strip_tags($object->getDescription());
+										//$message .= "\n\n" . $object->getURL();
+										try {
+											$facebookAPI->setFileUploadSupport(true);
+											$response = $facebookAPI->api(
+												'/me/photos/',
+												'post',
+												array(
+													'message' => $message,
+													'url' => $attachment['url']
+												)
+											);
+											if (!empty($response['id'])) {
+												$object->setPosseLink('facebook','https://facebook.com/' . $response['id']);
+												$object->save();
+											}
+										}
+										catch (\FacebookApiException $e) {
+											error_log('Could not post image to Facebook: ' . $e->getMessage());
+										}
+									}
+								}
+							}
+						}
+					}
                 });
             }
 
